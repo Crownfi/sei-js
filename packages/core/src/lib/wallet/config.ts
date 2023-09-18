53ff4c78096513eebbda9713d092e501476f41af
ARitz Cracker <aritz@aritzcracker.ca>
2023-09-18 21:16:58 +0000

    Update sei-js-core: * Rework browser wallet detection * Removed unused eslint config * update package.json * add more files to .npmignore

--- packages/core/src/lib/wallet/config.ts
+++ packages/core/src/lib/wallet/config.ts
@@ -2,102 +2,128 @@ import { ChainConfig, ChainInfo, Currency } from './types';
 import { AccountData, OfflineSigner } from '@cosmjs/proto-signing';
 import { StdSignature } from '@cosmjs/amino';
 
-export interface SeiWallet {
-	walletInfo: {
-		windowKey: string;
-		name: string;
-		icon: string;
-		website: string;
-	};
-	getOfflineSigner: (chainId: string) => Promise<OfflineSigner | undefined>;
-	getAccounts: (chainId: string) => Promise<readonly AccountData[]>;
-	connect: (chainId: string) => Promise<void>;
-	disconnect: (chainId: string) => Promise<void>;
-	suggestChain?: (config: ChainConfig) => Promise<void>;
-	signArbitrary?: (chainId: string, signer: string, message: string) => Promise<StdSignature | undefined>;
-	verifyArbitrary?: (chainId: string, signingAddress: string, data: string, signature: StdSignature) => Promise<boolean>;
-	isMobileSupported: boolean;
+export interface SeiProviderCommon {
+	getOfflineSignerAuto: (chainId: string) => Promise<OfflineSigner | undefined>;
+	enable: (chainId: string) => Promise<void>
+	disable: (chainId: string) => Promise<void>
+	signArbitrary?: (chainId: string, signer: string, message: string) => Promise<StdSignature | undefined>
+	verifyArbitrary?: (chainId: string, signingAddress: string, data: string, signature: StdSignature) => Promise<boolean>
+	experimentalSuggestChain?: (config: ChainConfig) => Promise<void>
 }
 
-export const FIN_WALLET: SeiWallet = {
-	getAccounts: async (chainId) => {
-		const offlineSigner = await window?.['fin']?.getOfflineSignerAuto(chainId);
-		return offlineSigner?.getAccounts() || [];
-	},
-	connect: async (chainId) => await window?.['fin']?.enable(chainId),
-	disconnect: async (chainId) => await window?.['fin']?.disable(chainId),
-	getOfflineSigner: async (chainId) => window?.['fin']?.getOfflineSignerAuto(chainId),
-	signArbitrary: async (chainId, signer, message) => window?.['fin']?.signArbitrary(chainId, signer, message),
-	verifyArbitrary: async (chainId, signingAddress, data, signature) => window?.['fin']?.verifyArbitrary(chainId, signingAddress, data, signature),
-	walletInfo: {
-		windowKey: 'fin',
-		name: 'Fin',
-		website: 'https://finwallet.com',
-		icon: 'https://sei-js-assets.s3.us-west-2.amazonaws.com/fin.png'
-	},
-	isMobileSupported: false
-};
+export const KNOWN_SEI_PROVIDERS = ["fin", "compass", "keplr", "leap"] as const;
+export type KnownSeiProviders = typeof KNOWN_SEI_PROVIDERS[number];
+export interface SeiProviderInfo<S extends string> {
+	windowKey: S;
+	name: string;
+	icon: string;
+	website: string;
+}
 
-export const COMPASS_WALLET: SeiWallet = {
-	getAccounts: async (chainId) => {
-		const offlineSigner = await window?.['compass']?.getOfflineSignerAuto(chainId);
-		return offlineSigner?.getAccounts() || [];
+export const KNOWN_SEI_PROVIDER_INFO = {
+	"fin": {
+		windowKey: "fin" as const,
+		name: "Fin",
+		website: "https://finwallet.com",
+		icon: "https://sei-js-assets.s3.us-west-2.amazonaws.com/fin.png",
 	},
-	connect: async (chainId) => await window?.['compass']?.enable(chainId),
-	disconnect: async (chainId) => await window?.['compass']?.disable(chainId),
-	getOfflineSigner: async (chainId) => window?.['compass']?.getOfflineSignerAuto(chainId),
-	signArbitrary: async (chainId, signer, message) => window?.['compass']?.signArbitrary(chainId, signer, message),
-	verifyArbitrary: async (chainId, signingAddress, data, signature) => window?.['compass']?.verifyArbitrary(chainId, signingAddress, data, signature),
-	walletInfo: {
-		windowKey: 'compass',
-		name: 'Compass',
-		website: 'https://chrome.google.com/webstore/detail/compass-wallet/anokgmphncpekkhclmingpimjmcooifb',
-		icon: 'https://sei-js-assets.s3.us-west-2.amazonaws.com/compass.png'
+	"compass": {
+		windowKey: "compass" as const,
+		name: "Compass",
+		website: "https://chrome.google.com/webstore/detail/compass-wallet/anokgmphncpekkhclmingpimjmcooifb",
+		icon: "https://sei-js-assets.s3.us-west-2.amazonaws.com/compass.png"
 	},
-	isMobileSupported: true
-};
+	"keplr": {
+		windowKey: "keplr" as const,
+		name: "Keplr",
+		website: "https://www.keplr.app/download",
+		icon: "https://sei-js-assets.s3.us-west-2.amazonaws.com/keplr.png"
+	},
+	"leap": {
+		windowKey: "leap" as const,
+		name: "Leap",
+		website: "https://www.leapwallet.io/download",
+		icon: "https://sei-js-assets.s3.us-west-2.amazonaws.com/leap.png"
+	}
+}
 
-export const KEPLR_WALLET: SeiWallet = {
-	getAccounts: async (chainId) => {
-		const offlineSigner = await window?.['keplr']?.getOfflineSignerAuto(chainId);
-		return offlineSigner?.getAccounts() || [];
-	},
-	connect: async (chainId) => await window?.['keplr']?.enable(chainId),
-	disconnect: async (chainId) => await window?.['keplr']?.disable(chainId),
-	getOfflineSigner: async (chainId) => window?.['keplr']?.getOfflineSignerAuto(chainId),
-	signArbitrary: async (chainId, signer, message) => window?.['keplr']?.signArbitrary(chainId, signer, message),
-	verifyArbitrary: async (chainId, signingAddress, data, signature) => window?.['keplr']?.verifyArbitrary(chainId, signingAddress, data, signature),
-	suggestChain: async (config) => window?.['keplr']?.experimentalSuggestChain(config),
-	walletInfo: {
-		windowKey: 'keplr',
-		name: 'Keplr',
-		website: 'https://www.keplr.app/download',
-		icon: 'https://sei-js-assets.s3.us-west-2.amazonaws.com/keplr.png'
-	},
-	isMobileSupported: false
-};
+for (const k of KNOWN_SEI_PROVIDERS) {
+	Object.freeze(KNOWN_SEI_PROVIDER_INFO[k]); // When I say const I MEAN CONST!!
+}
 
-export const LEAP_WALLET: SeiWallet = {
-	getAccounts: async (chainId) => {
-		const offlineSigner = await window?.['leap']?.getOfflineSignerAuto(chainId);
-		return offlineSigner?.getAccounts() || [];
-	},
-	connect: async (chainId) => await window?.['leap']?.enable(chainId),
-	disconnect: async (chainId) => await window?.['leap']?.disable(chainId),
-	getOfflineSigner: async (chainId) => window?.['leap']?.getOfflineSignerAuto(chainId),
-	signArbitrary: async (chainId, signer, message) => window?.['leap']?.signArbitrary(chainId, signer, message),
-	verifyArbitrary: async (chainId, signingAddress, data, signature) => window?.['leap']?.verifyArbitrary(chainId, signingAddress, data, signature),
-	suggestChain: async (config) => window?.['leap']?.experimentalSuggestChain(config),
-	walletInfo: {
-		windowKey: 'leap',
-		name: 'Leap',
-		website: 'https://www.leapwallet.io/download',
-		icon: 'https://sei-js-assets.s3.us-west-2.amazonaws.com/leap.png'
-	},
-	isMobileSupported: true
-};
-
-export const SUPPORTED_WALLETS: SeiWallet[] = [COMPASS_WALLET, FIN_WALLET, LEAP_WALLET, KEPLR_WALLET];
+export class SeiWallet {
+	walletInfo: SeiProviderInfo<KnownSeiProviders>
+	constructor(providerId: KnownSeiProviders) {
+		this.walletInfo = KNOWN_SEI_PROVIDER_INFO[providerId];
+		if (typeof window == "undefined") {
+			throw new Error("Not running in browser");
+		}
+		this.getProvider(); // Error out if wallet isn't installed
+	}
+	static discoveredWallets(): {[k in KnownSeiProviders]: boolean} {
+		if (typeof window == "undefined") {
+			throw new Error("Not running in browser");
+		}
+		const result = {} as any;
+		for (const k of KNOWN_SEI_PROVIDERS) {
+			result[k] = (window as any)[k] != undefined;
+		}
+		return result;
+	}
+	static discoveredWalletList(): KnownSeiProviders[] {
+		if (typeof window == "undefined") {
+			throw new Error("Not running in browser");
+		}
+		const result: KnownSeiProviders[] = [];
+		for (const k of KNOWN_SEI_PROVIDERS) {
+			if ((window as any)[k] != undefined) {
+				result.push(k);
+			}
+		}
+		return result;
+	}
+	getProvider(): SeiProviderCommon {
+		if ((window as any)[this.walletInfo.windowKey] == undefined) {
+			throw new Error(this.walletInfo.name + " wallet not found");
+		}
+		return (window as any)[this.walletInfo.windowKey];
+	}
+	async getOfflineSigner(chainId: string): Promise<OfflineSigner | undefined> {
+		return this.getProvider().getOfflineSignerAuto(chainId);
+	}
+	async getAccounts(chainId: string): Promise<readonly AccountData[]> {
+		const offlineSigner = await this.getProvider().getOfflineSignerAuto(chainId);
+		return offlineSigner?.getAccounts() || []
+	};
+	async connect(chainId: string): Promise<void> {
+		await this.getProvider().enable(chainId);
+	};
+	async disconnect(chainId: string): Promise<void> {
+		await this.getProvider().disable(chainId);
+	};
+	async suggestChain(config: ChainConfig): Promise<void> {
+		const provider = this.getProvider();
+		if (provider.experimentalSuggestChain != undefined) {
+			await provider.experimentalSuggestChain(config);
+		}
+	};
+	async signArbitrary(chainId: string, signer: string, message: string): Promise<StdSignature | undefined> {
+		const provider = this.getProvider();
+		if (provider.signArbitrary != undefined) {
+			return await provider.signArbitrary(chainId, signer, message);
+		}
+	};
+	/**
+	 * Be careful using this, a malicious client could just return true 100% of the time 
+	 */
+	async verifyArbitrary(chainId: string, signingAddress: string, data: string, signature: StdSignature): Promise<boolean> {
+		const provider = this.getProvider();
+		if (provider.verifyArbitrary != undefined) {
+			return await provider.verifyArbitrary(chainId, signingAddress, data, signature);
+		}
+		return false;
+	};
+}
 
 const DEFAULT_CHAIN_INFO = {
 	chainName: 'Sei',
