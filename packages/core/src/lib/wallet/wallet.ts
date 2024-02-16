9e6bc9568fbbd8be207ba806f22b5c7d9a01fdb3
ARitz Cracker <aritz@aritzcracker.ca>
2024-02-16 20:27:21 +0000

    update sei-js-core: * Allow for exported wallet functions to replicate Metamask key derivation behaviour

--- packages/core/src/lib/wallet/wallet.ts
+++ packages/core/src/lib/wallet/wallet.ts
@@ -1,18 +1,31 @@
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
 import { HdPath, stringToPath } from '@cosmjs/crypto';
 
-const getHdPath = (accountIndex = 0): HdPath => {
-	const stringPath = `m/44'/118'/0'/0/${accountIndex}`;
+export enum HdCoinType {
+	Sei = 118,
+	Ethereum = 60
+}
+
+function getHdPath(accountIndex: number = 0, coinType: HdCoinType | number = HdCoinType.Sei): HdPath {
+	const stringPath = `m/44'/${coinType}'/0'/0/${accountIndex}`;
 	return stringToPath(stringPath);
 };
 
-export const generateWallet = async (mnemonicLength: 12 | 15 | 18 | 21 | 24 = 12, accountIndex?: number): Promise<DirectSecp256k1HdWallet> => {
+export async function generateWallet(
+	mnemonicLength: 12 | 15 | 18 | 21 | 24 = 12,
+	accountIndex?: number,
+	coinType?: HdCoinType | number
+): Promise<DirectSecp256k1HdWallet> {
 	return await DirectSecp256k1HdWallet.generate(mnemonicLength, {
 		prefix: 'sei',
-		hdPaths: [getHdPath(accountIndex)]
+		hdPaths: [getHdPath(accountIndex, coinType)]
 	});
 };
 
-export const restoreWallet = async (seedPhrase: string, accountIndex?: number): Promise<DirectSecp256k1HdWallet> => {
-	return await DirectSecp256k1HdWallet.fromMnemonic(seedPhrase, { prefix: 'sei', hdPaths: [getHdPath(accountIndex)] });
+export async function restoreWallet(
+	seedPhrase: string,
+	accountIndex?: number,
+	coinType?: HdCoinType | number
+): Promise<DirectSecp256k1HdWallet> {
+	return await DirectSecp256k1HdWallet.fromMnemonic(seedPhrase, { prefix: 'sei', hdPaths: [getHdPath(accountIndex, coinType)] });
 };
